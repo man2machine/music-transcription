79ceb0a327e94d09416998a55745b5b100f9fb82
Sathwik Karnik <skarnik2012@gmail.com>
2020-12-07 02:24:54 -0500

    fixed a few mistakes, calc'd avg stats

--- noteify/core/calc_score.py
+++ noteify/core/calc_score.py
@@ -16,7 +16,37 @@ OFFSET_RATIO = 0.2
 OFFSET_MIN_TOLERANCE = 0.05
 ONSET_TOLERANCE = 0.05
 
-def calculate_score_per_song(model, dataloader, device, ref_midi_notes, est_midi_notes):
+def get_note_events():
+    return None
+
+def get_est_on_offs():
+    return None
+
+def get_est_midi_notes():
+    return None
+
+def calculate_average_stats(list_stats):
+    if len(list_stats) == 0:
+        return {}
+    
+    average_stats = {}
+    average_stats['frame_precision'] = np.average([stat['frame_precision'] for stat in list_stats])
+    average_stats['frame_recall']    = np.average([stat['frame_recall'] for stat in list_stats])
+    average_stats['frame_f1']        = np.average([stat['frame_f1'] for stat in list_stats])
+    
+    if 'note_precision' in list_stats[0].keys():
+        average_stats['note_precision']  = np.average([stat['note_precision'] for stat in list_stats])
+        average_stats['note_recall']     = np.average([stat['note_recall'] for stat in list_stats])
+        average_stats['note_f1']         = np.average([stat['note_f1'] for stat in list_stats])
+    
+    if 'reg_note_precision' in list_stats[0].keys():
+        average_stats['reg_note_precision']  = np.average([stat['reg_note_precision'] for stat in list_stats])
+        average_stats['reg_note_recall']     = np.average([stat['reg_note_recall'] for stat in list_stats])
+        average_stats['reg_note_f1']         = np.average([stat['reg_note_f1'] for stat in list_stats])
+    
+    return average_stats
+
+def calculate_score_per_song(model, dataloader, device):
     """
     Calculate score from paper per song.
     """
@@ -43,15 +73,15 @@ def calculate_score_per_song(model, dataloader, device, ref_midi_notes, est_midi
         
     
     if 'onset_output' in outputs.keys() and 'offset_output' in outputs.keys():
-        ref_on_off_pairs = np.array(list(zip(targets['onset_roll'].flatten(), targets['onset_roll'].flatten())))
-        est_on_offs = np.array(list(zip(outputs['onset_roll'].flatten(), targets['outputs'].flatten())))
+        ref_on_off_pairs = np.array([[event['onset_time'], event['offset_time']] for event in get_note_events()])
+        ref_midi_notes = np.array([event['midi_note'] for event in get_note_events()])
         
         note_precision, note_recall, note_f1, _ = \
                     mir_eval.transcription.precision_recall_f1_overlap(
                         ref_intervals=ref_on_off_pairs, 
                         ref_pitches=librosa.note_to_hz(ref_midi_notes), 
-                        est_intervals=est_on_offs, 
-                        est_pitches=librosa.note_to_hz(est_midi_notes), 
+                        est_intervals=get_est_on_offs(), 
+                        est_pitches=librosa.note_to_hz(get_est_midi_notes()), 
                         onset_tolerance=ONSET_TOLERANCE, 
                         offset_ratio=OFFSET_RATIO, 
                         offset_min_tolerance=OFFSET_MIN_TOLERANCE)
@@ -60,16 +90,16 @@ def calculate_score_per_song(model, dataloader, device, ref_midi_notes, est_midi
         stats['note_recall'] = note_recall
         stats['note_f1'] = note_f1
         
-    if 'reg_onset_output' and 'reg_offset_output' in outputs.keys():
-        ref_on_off_pairs = np.array(list(zip(targets['reg_onset_output'].flatten(), targets['reg_onset_roll'].flatten())))
-        est_on_offs = np.array(list(zip(outputs['reg_offset_output'].flatten(), targets['reg_onset_roll'].flatten())))
+    if 'reg_onset_output' in outputs.keys() and 'reg_offset_output' in outputs.keys():
+        ref_on_off_pairs = np.array([[event['onset_time'], event['offset_time']] for event in get_note_events()])
+        ref_midi_notes = np.array([event['midi_note'] for event in get_note_events()])
         
         note_precision, note_recall, note_f1, _ = \
                     mir_eval.transcription.precision_recall_f1_overlap(
                         ref_intervals=ref_on_off_pairs, 
                         ref_pitches=librosa.note_to_hz(ref_midi_notes), 
-                        est_intervals=est_on_offs, 
-                        est_pitches=librosa.note_to_hz(est_midi_notes), 
+                        est_intervals=get_est_on_offs(), 
+                        est_pitches=librosa.note_to_hz(get_est_midi_notes()), 
                         onset_tolerance=ONSET_TOLERANCE, 
                         offset_ratio=OFFSET_RATIO, 
                         offset_min_tolerance=OFFSET_MIN_TOLERANCE)
