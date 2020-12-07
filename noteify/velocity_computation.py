fixed velocity computation to get rid of outliers

noteify/velocity_computation.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import librosa
 
 MAX_VELOCITY = 127
+EPSILON = 1e-5
 
 def get_velocities_from_spectrogram(cqt, frequency_bins, start_and_end_buckets, num_time_steps = 10):
     """
@@ -19,13 +20,17 @@ def get_velocities_from_spectrogram(cqt, frequency_bins, start_and_end_buckets, 
     (start time bucket, end time bucket)
 
     """
-    
-    max_cqt_value = np.max(cqt)
-    modified_cqt = cqt/max_cqt_value
     start_and_end_buckets = np.array(start_and_end_buckets)
     ranges = np.concatenate([[np.arange(num_time_steps)] for i in range(len(start_and_end_buckets))], 0)
     start_buckets = np.transpose([start_and_end_buckets[:, 0]])
     frequency_bins_transposed = np.transpose([frequency_bins])
+    
+    cqt_mean = np.mean(cqt[np.where(cqt > EPSILON)])
+    cqt_std = np.std(cqt[np.where(cqt > EPSILON)])
+    modified_cqt = np.zeros(np.shape(cqt))
+    indices = np.where(abs(cqt - cqt_mean) < 3 * cqt_std)
+    modified_cqt[indices] = cqt[indices] / np.max(cqt)
+    
     velocities = np.max(modified_cqt[frequency_bins_transposed, start_buckets + ranges], axis=(1,)) * MAX_VELOCITY
     velocities = np.array(velocities, dtype=int)
     